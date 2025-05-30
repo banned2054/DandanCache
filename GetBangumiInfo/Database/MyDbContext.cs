ce43d0c010f182e169513705a2bd40b8f89bfb2a
banned2054 <banned2054@163.com>
2025-05-30 15:02:57 +0800

    revert(database): 回滚到之前的数据库设置。

--- GetBangumiInfo/Database/MyDbContext.cs
+++ GetBangumiInfo/Database/MyDbContext.cs
@@ -1,7 +1,5 @@
-using System.Net;
 using GetBangumiInfo.Models.Database;
 using Microsoft.EntityFrameworkCore;
-using System.Net.Sockets;
 
 namespace GetBangumiInfo.Database;
 
@@ -19,22 +17,12 @@ public class MyDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var connectHost    = Environment.GetEnvironmentVariable("DatabaseHost");
         var connectSetting = Environment.GetEnvironmentVariable("DatabaseConnectSetting");
-        if (string.IsNullOrEmpty(connectHost) || string.IsNullOrEmpty(connectSetting)) return;
-
-        var hostEntries = Dns.GetHostAddresses(connectHost);
-        var ipv4Address = hostEntries.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
-
-        if (ipv4Address == null)
+        if (string.IsNullOrEmpty(connectSetting))
         {
-            Console.WriteLine("Failed to resolve IPv4 address.");
-            return;
+            throw new Exception("Database Connect Setting Not Found.");
         }
 
-        // 构造完整连接字符串：Host=xxx + connectSetting（后半部分）
-        var fullConnectionString = $"Host={ipv4Address};{connectSetting}";
-
-        optionsBuilder.UseNpgsql(fullConnectionString);
+        optionsBuilder.UseNpgsql(connectSetting);
     }
 }
