a3ee29d5b78ce1bb0862619ab41296e2760aff4d
banned2054 <banned2054@163.com>
2025-05-31 11:28:12 +0800

    feat(test): 添加环境变量，用于本地测试

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,3 +1,4 @@
+using GetBangumiInfo.Database;
 using GetBangumiInfo.Utils;
 
 namespace Test;
@@ -6,9 +7,19 @@ internal class Program
 {
     private static void Main()
     {
-        var todayWeekday     = DateUtils.GetBeijingWeekday() + 1;
-        var yesterdayWeekday = (todayWeekday + 6) % 7;
-        Console.WriteLine(todayWeekday);
-        Console.WriteLine(yesterdayWeekday);
+        DotNetEnv.Env.Load();
+
+        var db            = new MyDbContext();
+        var subjectIdList = db.EpisodeList.Select(e => e.SubjectId).Distinct();
+        foreach (var subjectId in subjectIdList)
+        {
+            var element = BangumiUtils.GetSubjectInfo(subjectId);
+            if (element == null) continue;
+            if (!element.Value.TryGetProperty("date", out var dateProp)) continue;
+            var dateStr = dateProp.GetString();
+            if (!DateTime.TryParse(dateStr, out var date)) continue;
+            var weekday = ((int)date.DayOfWeek + 6) % 7 + 1; // 周一=1，周日=7
+            Console.WriteLine($"Weekday: {weekday}");
+        }
     }
 }
